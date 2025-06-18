523fa624c1a87d3e109c52afcebc18aaaf97a64b
lightzane <jp.eaguilar@gmail.com>
2025-06-18 18:28:07 +0800

    add github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/try-vue3-vuefire',
   plugins: [
     vue(),
     vueDevTools(),
